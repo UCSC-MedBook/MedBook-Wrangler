935bd7417cbc7e2471c52acd7137ceab3d7a3851
rbaertsch <baertsch@soe.ucsc.edu>
2015-05-20 09:18:25 -0700

    login now works in R.

--- R/medbooklib.R
+++ R/medbooklib.R
@@ -4,12 +4,21 @@ library(methods)
 library(httr)
 library(stringr)
 MedBookConnection <- function(user="", password="") {
-        env = Sys.getenv()
-        if ((str_length(user) >1  && str_length(password) >1) || ("MEDBOOKUSER" %in% names(env) && "MEDBOOKPASSWORD" %in% names(env))) {
-            if (str_length(user) < 2)     { user <- paste0(env["MEDBOOKUSER"]) }
-            if (str_length(password) < 2) { password <- paste0(env["MEDBOOKPASSWORD"]); }
-            if ("MEDBOOKSERVER" %in% names(env)) { server = paste0(env["MEDBOOKSERVER"]); }
-            else { server = "http://localhost"; }
+       env = Sys.getenv()
+       if ((str_length(user) >1  && str_length(password) >1) 
+               || ("MEDBOOKUSER" %in% names(env) && "MEDBOOKPASSWORD" %in% names(env))) {
+            if (str_length(user) < 2)     { 
+                user <- paste0(env["MEDBOOKUSER"]) 
+            }
+            if (str_length(password) < 2) { 
+                password <- paste0(env["MEDBOOKPASSWORD"]); 
+            }
+            if ("MEDBOOKSERVER" %in% names(env)) { 
+                server <<- paste0(env["MEDBOOKSERVER"]); 
+            }
+            else { 
+                server <<- "https://medbook.ucsc.edu"; 
+            }
 #            if (server[-1] == "/") {
 #                server = server[0:-1]
 #            }
@@ -17,32 +26,38 @@ MedBookConnection <- function(user="", password="") {
             payload = list(user = user, password = password)
             full_url = paste0(server , "/data/api/login")
             print(c("server:",full_url))
-            credentials = POST(full_url, body=payload, encode = "json")
-            print(c("credentials:",credentials))
+            credentials = POST(full_url, body=payload, encode = "form")
+#print(c("credentials:",credentials))
             stopifnot( credentials != NULL)
             stopifnot( credentials$status_code == 200)
                  # "Could not login: " + toString(credentials.status_code) + " " + credentials.reason + ", check MEDBOOKUSER && MEDBOOKPASSWORD for server:" + server)
-            result = content(credentials, "text")
-            print(paste0('result:',str(result)))
-#stopifnot( obj != NULL && "data" %in% obj && obj["data"] != NULL) 
+            obj = content(credentials)
+            cred <- obj$data
+            credentials <<- cred
+            print(paste('obj$data',cred$authToken))
+            print(names(cred))
+            stopifnot( obj != NULL ) 
+            stopifnot( obj$data != NULL) 
+            stopifnot( "authToken" %in% names(cred) ) 
+            stopifnot( "userId" %in% names(cred) ) 
             #"Could not login: badly formed response from Meteor Restivius" + (credentials) + toString(obj);
-#            credentials = obj["data"];
-#            stopifnot( credentials["authToken"])
-#            stopifnot(credentials["userId"])
-            return(result);
+#credentials = obj["data"];
         }
-        else { print( "Please pass username and password or set MEDBOOKUSER && MEDBOOKPASSWORD in your environment variables. You will also need to set MEDBOOKSERVER if you are not using medbook.ucsc.edu");
+        else { 
+            print( "Please pass username and password or set MEDBOOKUSER && MEDBOOKPASSWORD in your environment variables. You will also need to set MEDBOOKSERVER if you are not using medbook.ucsc.edu");
         }
+        cred
 }
 
-find <- function (collName, query=NULL, params){
-        stopifnot( query==NULL || len(params)==0)
+find <- function (collName, query="", params=""){
+        stopifnot( str_length(query)==0 || str_length(params)==0)
 # "MedBook.find(): Use either a Mongo query or keywords, but not both"
 
-        headers = list("X-Auth-Token"= credentials["authToken"], "X-User-Id"= credentials["userId"]);
+        headers = list("X-Auth-Token"= credentials$authToken, "X-User-Id"= credentials$userId);
+        j_headers = toJSON(headers)
+        print(paste('headers',j_headers))
 
-        print(c("find: query ",query))
-        if (query) {
+        if (str_length(query)>1) {
             params = list( 'json'=query );
         }
         else {
@@ -54,15 +69,17 @@ find <- function (collName, query=NULL, params){
             }
         }
 
-        url = server + "/data/api/" + collName;
-        wrapped = GET(url, params=params, config=toJSON(headers));
+        url = paste0(server , "/data/api/", collName)
+        wrapped = GET(url, add_headers(j_headers));
+#wrapped = GET(url, params=params, config=j_headers);
+        print(content(wrapped))
         stopifnot( wrapped);
         result1 = toJSON(wrapped["data"]);
         stopifnot( result1);
         return (result1);
 }
 insert <- function(collection, obj){
-        url = server + "/data/api/" + collection
+        url = paste0(server , "/data/api/", collection)
         headers = list("X-Auth-Token"= credentials["authToken"], "X-User-Id"= credentials["userId"]);
         response = POST(url, json=obj, config=toJSON(headers));
         if (response.status_code == 200) {
@@ -79,7 +96,7 @@ update <- function(collection, obj){
             print ( c("update needs a valid _id  field:\n" , toString(obj) ));
             return(-2)
         }
-        url = server + "/data/api/" + collection  #   + "/" + obj["_id"] + "/";
+        url = paste0(server , "/data/api/", collection)
         headers = list("X-Auth-Token"= credentials["authToken"], "X-User-Id"= credentials["userId"]);
         response = PUT(url, json=obj, config=toJSON(headers));
         if (response.status_code == 200) {
@@ -94,6 +111,7 @@ update <- function(collection, obj){
 
 test <- function(){
     medbook = MedBookConnection();
+    print(paste('authToken:',medbook$authToken))
 
     tests = 0;
     result = tryCatch({
@@ -102,29 +120,30 @@ test <- function(){
     }, error = function(e) {
         tests <- tests + 1;
     })
+    print("find clinical ")
     data = find("Clinical_Info");
     print(paste0("find clinical ", data))
-    stopifnot( len(data) > 1)
+    stopifnot( length(data) > 1)
     tests <- tests + 1;
 
     data = find("Expression2", toJSON(list( "Study_ID"= "prad_wcdt", "gene"= "BRCA1" )));
-    stopifnot( len(data) == 1)
+    stopifnot( length(data) == 1)
     tests <- tests + 1;
 
 #data = find("Expression2", { "Study_ID": {"$in": [ "prad_tcga", "prad_wcdt"] },  "gene": { "$in": ["EGFR","BRCA1"]}});
-#    stopifnot( len(data) == 2)
+#    stopifnot( length(data) == 2)
 #    tests <- tests + 1;
 
     data = find("Expression2", list(gene="EGFR"));
-    stopifnot( len(data) == 1)
+    stopifnot( length(data) == 1)
     tests <- tests + 1;
 
     data = find("Expression2", list(gene=list("EGFR","BRCA1")));
-    stopifnot( len(data) == 2)
+    stopifnot( length(data) == 2)
     tests <- tests + 1;
 
     data = find("Expression2", list(gene=list("EGFR","BRCA1"), Study_ID= "prad_wcdt"));
-    stopifnot( len(data) == 2)
+    stopifnot( length(data) == 2)
     tests <- tests + 1;
 
     contrast = insert("contrast", list(
@@ -136,7 +155,7 @@ test <- function(){
                 list2=list("DTB-004", "DTB-005", "DTB-006"),
                 collaborations="prad_wcdt"));
 
-    stopifnot( len(contrast))
+    stopifnot( length(contrast))
     tests <- tests + 1;
 
     sig  = insert("signature", list(
@@ -145,14 +164,14 @@ test <- function(){
                 version=1, 
                 contrast=contrast["_id"], 
                 signature=list( "AR" : list( "weight" : 3.3 ),  "ASCL1" : list( "weight" : "0.042114584368837" ))));
-    stopifnot( len(sig))
+    stopifnot( length(sig))
     tests <- tests + 1;
 
     sig["signature"] = list( "AR" : list( "weight" : 1.0 ), "ASCL1" : list( "weight" : "1.0" ));
     sig  = medbook("signature", sig);
 
     
-    stopifnot( len(sig))
+    stopifnot( length(sig))
     tests <- tests + 1;
 
     print(c("success tests", tests, "passed"))

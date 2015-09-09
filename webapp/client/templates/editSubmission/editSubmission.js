b4f592d73dafefaef9cfd9148cc5888ca0c7a443
mokolodi1 <mokolodi1@gmail.com>
2015-09-09 16:19:38 -0700

    redid submission files UI with bootstrap

--- webapp/client/templates/editSubmission/editSubmission.js
+++ webapp/client/templates/editSubmission/editSubmission.js
@@ -1,23 +1,31 @@
-Template.submissionFileList.events({
+Template.submissionFiles.helpers({
+  fileContextualClass: function () {
+    switch (this.status) {
+      case "done":
+        return "list-group-item-success";
+      case "creating": case "uploading": case "saving":
+        return "list-group-item-warning";
+      case "processing":
+        return "list-group-item-info";
+      case "error":
+        return "list-group-item-danger";
+      default:
+
+    }
+  },
+});
+
+Template.submissionFiles.events({
+  // manipulating already uploaded files
+
   // click the remove button for a specific file
   "click .remove-this-file": function(event, instance) {
     Meteor.call("removeFileFromSubmission", instance.data._id,
         this.file_id);
   },
-});
 
-// defined out here because it's used in two helpers
-// (_.partial used within the functions)
-function fullInsertCallback (submissionId, error, fileObject) {
-  if (error) {
-    console.log("error:", error);
-  } else {
-    Meteor.call("addFileToSubmission", submissionId, fileObject._id,
-        fileObject.original.name);
-  }
-}
+  // adding new files
 
-Template.fileUploader.events({
   // when they click the button to add a file
   "click #add-files-button": function (event, instance) {
     $("#upload-files-input").click();
@@ -63,6 +71,17 @@ Template.fileUploader.events({
     });
   },
 });
+
+// defined out here because it's used in two helpers
+// (_.partial used within the functions)
+function fullInsertCallback (submissionId, error, fileObject) {
+  if (error) {
+    console.log("error:", error);
+  } else {
+    Meteor.call("addFileToSubmission", submissionId, fileObject._id,
+        fileObject.original.name);
+  }
+}
 
 Template.listErrorsAndSubmit.helpers({
   hasErrors: function () {

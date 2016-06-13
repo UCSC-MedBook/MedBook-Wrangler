6b22af761271379323c229b2697aa1908c6aa167
Ellen <ekephart@ucsc.edu>
2016-06-13 23:03:50 +0000

    Removes old-webapp dir
    No longer in use; ok to delete per Teo.

--- old-webapp/client/components/eulaPage/eulaPage.js
+++
@@ -1,9 +0,0 @@
-Router.route('/data/eula', {
-  template: 'eulaPage',
-  name: 'eulaPage'
-});
-
-Router.route('/data/terms-of-use', {
-  template: 'eulaPage',
-  name: 'eulaPage'
-});
--- old-webapp/client/components/privacyPage/privacyPage.js
+++
@@ -1,4 +0,0 @@
-Router.route("/data/privacy",{
-  template: 'privacyPage',
-  name: 'privacyPage'
-});

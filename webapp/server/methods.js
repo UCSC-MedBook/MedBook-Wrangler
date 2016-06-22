82a43e88e101efb1386e156d02d55b1ddb63eb01
Teo Fleming <mokolodi1@gmail.com>
2016-06-22 11:44:33 -0700

    small bugfixes after code review

--- webapp/server/methods.js
+++ webapp/server/methods.js
@@ -1,54 +1,16 @@
 Meteor.methods({
-  removeTestingData: function () {
-    var user = MedBook.ensureUser(Meteor.userId());
-
-    if (user.getCollaborations().indexOf("testing") >= 0) {
-      console.log("removing testing data");
-
-      GeneExpression.remove({
-        collaborations: "testing",
-      });
-
-      Expression2.remove({
-        Collaborations: "testing",
-      });
-
-      IsoformExpression.remove({
-        collaborations: "testing",
-      });
-
-      Studies.update({
-        collaborations: "testing",
-      }, {
-        $set: {
-          Sample_IDs: [],
-          Patient_IDs: [],
-        }
-      });
-
-      CRFs.remove({
-        CRF: "Clinical_Info",
-        Study_ID: "prad_test",
-      });
-
-      Contrasts.remove({
-        collaborations: "testing",
-      });
-
-      Signatures.remove({
-        collaborations: "testing",
-      });
-
-      return "done";
-    }
-
-    throw new Meteor.Error("you can't do that!");
-  },
   // Associate an existing blob with a
   // file submission
   attachBlobToSubmission: function (submissionID, blobID) {
     check(submissionID, String);
     check(blobID, String);
+
+    var user = MedBook.ensureUser(Meteor.userId());
+
+    // Grab the submission, make sure the user has access
+    existingSubmission = WranglerSubmissions.findOne({_id: submissionID});
+    user.ensureAccess(existingSubmission);
+    ensureEditable(existingSubmission);
 
     // Confirm blob exists and doesn't already
     // have submission metadata
@@ -57,25 +19,21 @@ Meteor.methods({
       throw new Meteor.Error("no-blob-found",
         "Couldn't find a blob with the provided _id.");
     }
-    console.log("found existingBlob", existingBlob);
-    if (existingBlob.metadata != undefined) {
-      throw new Meteor.Error("blob-has-metadata",
+
+    if (existingBlob.metadata && existingBlob.metadata.submission_id) {
+      throw new Meteor.Error("blob-already-attached-to-submission",
         "Provided blob already has submission-associated metadata");
     }
-    // Confirm submission exists
-    existingSubmission = WranglerSubmissions.findOne({_id: submissionID});
-    if(existingSubmission === undefined){
-      throw new Meteor.Error("no-submission-found",
-        "Couldn't find submission with provided _id");
-    }
+
     // Add the metadata to the blob associating it with user and submission.
-    var addMetadataModifier = {$set:
-      {
+    var addMetadataModifier = {
+      $set: {
+        // XXX: someone could claim a blob if they have the _id
         "metadata.user_id": Meteor.userId(),
         "metadata.submission_id":submissionID,
-        //"metadata.wrangler_upload": true,
         // Don't set wrangler_upload to true. This would cause the blob to be
         // deleted if the submission is deleted.
+        // "metadata.wrangler_upload": true,
       }
     };
     // Don't call Blobs.update with a callback but block instead.
@@ -85,6 +43,7 @@ Meteor.methods({
       throw new Meteor.Error("bad-blob-count-updated",
         "Expected to update 1 blob but updated " + updateRes + " instead.");
     }
+
     return existingBlob;
   },
 });
